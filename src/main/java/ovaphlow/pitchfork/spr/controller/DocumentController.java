e8948f75799442d4f0072db990b0d50ee8d4900b
ovaphlow <ovaphlow@live.com>
2022-04-27 21:33:30 +0800

    ...

--- src/main/java/ovaphlow/pitchfork/spr/controller/DocumentController.java
+++ src/main/java/ovaphlow/pitchfork/spr/controller/DocumentController.java
@@ -1,7 +1,6 @@
 package ovaphlow.pitchfork.spr.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,54 +12,40 @@ import ovaphlow.pitchfork.spr.utility.Snowflake;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/simple/biz")
 public class DocumentController {
 
+    private final DocumentMapper documentMapper;
     Document document;
-
     @Autowired
     private RedisUtil redisUtil;
-
-    private final DocumentMapper documentMapper;
 
     public DocumentController(DocumentMapper documentMapper) {
         this.documentMapper = documentMapper;
     }
 
     @RequestMapping(path = "/document/{id}/{title}", method = RequestMethod.GET)
-    public ResponseEntity<Document> getByIdTitle(@PathVariable("id") Long id,@PathVariable("title") String title){
-        Document result = documentMapper.filterByIdTitle(id,title);
+    public ResponseEntity<Document> getByIdTitle(@PathVariable("id") Long id, @PathVariable("title") String title) {
+        Document result = documentMapper.filterByIdTitle(id, title);
         return ResponseEntity.status(200).body(result);
     }
 
-
-
     @RequestMapping(path = "/document/{id}", method = RequestMethod.GET)
     public ResponseEntity<Document> getById(@PathVariable("id") Long id) {
-        Document result = documentMapper.filterById(id);
-
-
-
-        String key = "document"+id;
-        if(redisUtil.hasKey(key)) {
-            document = (Document)redisUtil.get(key);
-            System.out.println("search from cache");
-        }else{
+//        Document result = documentMapper.filterById(id);
+        String key = "document" + id;
+        if (redisUtil.hasKey(key)) {
+            document = (Document) redisUtil.get(key);
+//            System.out.println("search from cache");
+        } else {
             document = documentMapper.filterById(id);
-            System.out.println("search from database");
-            System.out.println(redisUtil.set(key,document) ? "success insert" : "error insert");
+//            System.out.println("search from database");
+            System.out.println(redisUtil.set(key, document) ? "success insert" : "error insert");
         }
-
-
-        return ResponseEntity.status(200).body(result);
-
-
+        return ResponseEntity.status(200).body(document);
     }
 
     // 技术员审核 需要前端提交 审核结果 班组 质检 审核状态（技术员审核） 技术员id 姓名 审核时间
@@ -73,14 +58,9 @@ public class DocumentController {
 
     @RequestMapping(path = "/document/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Document> remove(@PathVariable("id") Long id) {
-
-
         documentMapper.remove(id);
         return ResponseEntity.status(200).build();
-
-
     }
-
 
     @RequestMapping(path = "/document", method = RequestMethod.GET)
     public ResponseEntity<List<Document>> filter(@RequestParam(value = "option", defaultValue = "") String option,
@@ -95,7 +75,7 @@ public class DocumentController {
             String train = request.getParameter("train");
             String timeBegin = request.getParameter("timeBegin");
             String timeEnd = request.getParameter("timeEnd");
-            List<Document> result = documentMapper.filterByTimeRangeTrain(timeBegin,timeEnd,train);
+            List<Document> result = documentMapper.filterByTimeRangeTrain(timeBegin, timeEnd, train);
             return ResponseEntity.status(200).body(result);
         }
         return ResponseEntity.status(200).build();
@@ -109,17 +89,13 @@ public class DocumentController {
         return ResponseEntity.status(201).build();
     }
 
-
     @RequestMapping(path = "/upload", method = RequestMethod.POST)
     public String upload(@RequestBody MultipartFile file) throws IOException {
-
-        String oldName=file.getOriginalFilename();
-        String path="D:\\upload_files\\";
-        String filePath=path+oldName;
-        File newFile=new File(filePath);
+        String oldName = file.getOriginalFilename();
+        String path = "D:\\upload_files\\";
+        String filePath = path + oldName;
+        File newFile = new File(filePath);
         file.transferTo(newFile);
         return filePath;
-        
     }
-
 }
